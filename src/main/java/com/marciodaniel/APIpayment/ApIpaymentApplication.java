590f7af607cb71b381b2adfe6e29011850d6f7f1
Márcio Daniel(MADA) <moreiramarciodaniel@gmail.com>
2018-08-11 15:34:36 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/marciodaniel/APIpayment/ApIpaymentApplication.java
@@ -0,0 +1,12 @@
+package com.marciodaniel.APIpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIpaymentApplication.class, args);
+	}
+}
